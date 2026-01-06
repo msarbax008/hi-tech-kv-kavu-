2be06d152ebc3fbc9f13fff7ca19c4f0e74b142a
Firebase Studio <noreply@firebase.studio>
2026-01-06 11:44:02 +0000

    Initialized workspace with Firebase Studio

---
+++ src/bootstrap.js
@@ -0,0 +1,11 @@
+window["APP_TEMPLATE_BOOTSTRAP"] = {
+
+    firebase: {
+      apiKey: "AIzaSyAheo4x-NxkAeZ7FOY1yfvN9fL-Q7AUgRw",
+      authDomain: "hi-tech-a253c.firebaseapp.com",
+      projectId: "hi-tech-a253c",
+      storageBucket: "hi-tech-a253c.firebasestorage.app",
+      messagingSenderId: "133502135374",
+      appId: "1:133502135374:web:dbcf72add9392f7033271f",
+    }
+    };
